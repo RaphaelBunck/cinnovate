402d961b7df0fe2ba03845dde336583c34a620e5
Erik Baalbergen <erikbaalbergen@gmail.com>
2015-12-11 11:21:16 +0100

    Eerste versie van de hulpoproepen gemaakt.

--- patienten.php
+++ patienten.php
@@ -46,6 +46,49 @@ if(!$error)
 	}
 }
 
+$hulpoproepen = "";
+
+if(isset($_GET['id']))
+{
+	$id = $_GET['id'];
+	
+	if(is_numeric($id))
+		$query = "SELECT * FROM patients WHERE Patient_ID = " . $id;
+	else
+	{
+		$error = true;
+		$hulpoproepen = "<div style=\"width: 100%; height: 4em; font-size: 30px;\">Fout met ophalen van gegevens uit de database.</div>";
+	}
+} else
+{
+	$query = "SELECT * FROM patients";
+}
+
+if(!$error)
+{
+	$selectPDO = $pdo->query($query);
+	$selectData = $selectPDO->fetchAll();
+	foreach($selectData as $berichtText)
+	{
+		if(is_null($berichtText['profile_picture']))
+			$profilepicture = "generic-profile.png";
+		else
+			$profilepicture = $berichtText['profile_picture'];
+		
+		$hulpoproepen = $hulpoproepen . 
+					"<div style=\"width: 100%; height: 4em; position: relative; \">
+						<div style=\"color: black; position: absolute; top: 1.5em;\">
+							<a style=\"color: black; font-family: Arial, sans-serif; text-decoration: none;\" href=\"/patienten_info.php?id=" . $berichtText['ID'] . "\">
+								" . $berichtText['lName'] . ", " . $berichtText['fName'] . $berichtText['insertion'] . "
+							</a>
+						</div>
+						<div style=\"color: black; position: absolute; top: 0; right: 0;\">
+							<img style=\"height: 4em; width: 4em;\" src=\"./profile_picture/" . $profilepicture . "\">
+						</div>
+					</div><hr>";
+	}
+}
+
 ?>
 
 
@@ -58,6 +101,19 @@ if(!$error)
 		</title>
 	</head>
 	<body>
-		<?= $bericht?>
+		<div>
+			<div>
+				<h3>
+					Hulpoproepen
+				</h3>
+				<?= $hulpoproepen?>
+			</div>
+			<div>
+				<h3>
+					Patienten:
+				</h3>
+				<?= $bericht?>
+			</div>
+		</div>
 	</body>
 </html>
